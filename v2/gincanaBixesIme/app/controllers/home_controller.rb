432dd04549f24b48ac526fb773e1c152592e5887
vbatistella <victorabatistella@gmail.com>
2019-05-17 16:07:24 -0300

    Control Panel only for the adm and some changes to the home page

--- v2/gincanaBixesIme/app/controllers/home_controller.rb
+++ v2/gincanaBixesIme/app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def home
+    @tasks = Task.all
+    @user = current_user
   end
 end
--- v2/gincanaBixesIme/db/seeds.rb
+++ v2/gincanaBixesIme/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.new(email: "adm@adm", password: "admins", admin: true)
+admin.save!
+
+task1 = Task.new(name: "Palestras" , description: "Presença em palestras vale 50 pontos" , score_type: "Cada Palestra 50 pontos" , have_score: false , group: false , send: false, max_send: 10, have_feedback: true)
+task2 = Task.new(name: "Fantasia" , description: "Virá fantasiado todos os dias" , score_type: "Cada fantasia somar pontos" , have_score: true , group: false , send: false, max_send: 4, have_feedback: false)
+task1.save!
+task2.save!
