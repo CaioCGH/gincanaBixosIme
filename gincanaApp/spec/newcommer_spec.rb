aec7fb5f05415ed8dd759a64e34bdab4c839c006
PauloPaulo <ppqbambace@yahoo.com.br>
2019-06-25 00:39:13 -0300

    newcomwe tests

--- gincanaApp/spec/newcommer_spec.rb
+++ gincanaApp/spec/newcommer_spec.rb
@@ -16,5 +16,29 @@ RSpec.describe "Em Bixe" do
         #    newbixe = Newcomer.create(name: "William Shattner", telephone: "9821", course_id: 1, team_id: nil, user_id: 1, tutor: true, is_valid: false)
         #    newbixe.save!
         #    expect(Newcomer.count).to eq(count)
+        #end
     end
+    #context "tentar atualizar bixe" do
+    #	it "e conseguir" do
+    #		User.create(email: "roald.dahl@usp.br", password: "bixe123", admin: false)
+    #		newbixe = Newcomer.create(name: "Roald Dahl", telephone: "945956231", course_id: 6, team_id: nil, user_id: 7, tutor: false, is_valid: false)
+    #		Newcomer.update(team_id: 2, is_valid: true)
+    #		expect(newbixe.team_id == 2 and newbixe.is_valid == true).to be_truthy
+	#	end
+	#	it "e falhar" do
+	#		User.create(email: "roald.dahl@usp.br", password: "bixe123", admin: false)
+    #		newbixe = Newcomer.create(name: "Roald Dahl", telephone: "945956231", course_id: 6, team_id: nil, user_id: 7, tutor: false, is_valid: false)
+    #		Newcomer.update(telephone: "31")
+    #		expect(newbixe.team_id == 2 and newbixe.is_valid == true).to be_truthy
+    # 	end
+	#end
+	#context "tentar destruir bixe" do
+    #	it "e conseguir" do
+    #		user = User.create(email: "ricardo@usp.br", password: "bixe123", admin: false)
+    #		newbixe = Newcomer.create(name: "Ricardo aldo", telephone: "999999999", course_id: 6, team_id: nil, user_id: user.id , tutor: false, is_valid: false)
+    #		qnt = Newcomer.count
+    # 		Newcomer.destroy(newbixe.id)
+    #		expect(Newcomer.count).to eq(count - 1)
+	#	end
+	#end
 end
